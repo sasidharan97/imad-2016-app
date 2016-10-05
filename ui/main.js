1d05a430fd71fbb1c6c4183ac1d25bea6e481719
Sasidharan Muthusamy <mmsasidharan@gmail.com>
2016-10-05 16:54:36 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,24 @@
 // Counter Code
 
 var button = document.getElementById('counter');
-var counter = 0;
 
 button.onclick = function () {
     
+    var request = new XMLHttpRequest();
     
+    request.onreadystatechange = function () {
+       
+       if (request.readyState === XMLHttpRequest.DONE){
+           if (reuest.status === 200) {
+               var counter = request.responseText;
+               var span = document.getElementById('count');
+               span.innerHTML = counter.toString();
+           }
+       } 
+        
+    };
     
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    request.open('GET', 'http://sasidharan97.imad.hasura.io/counter', true);
+    request.send(null);
     
 };
